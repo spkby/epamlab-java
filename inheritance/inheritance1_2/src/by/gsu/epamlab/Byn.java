4718a3595271e90e6e9d165f4352944473b4e3d0
VlaShip <vlaship@bk.ru>
2018-04-04 22:49:33 +0300

    refactoring Round

--- inheritance/inheritance1_2/src/by/gsu/epamlab/Byn.java
+++ inheritance/inheritance1_2/src/by/gsu/epamlab/Byn.java
@@ -4,8 +4,6 @@ public class Byn implements Comparable<Byn> {
 
     private int value;
 
-    private static int[] tenPowD = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
-
     public Byn(int value) {
         this.value = value;
     }
@@ -26,80 +24,6 @@ public class Byn implements Comparable<Byn> {
         return this.value % 100;
     }
 
-    public Byn add(Byn byn) {
-        this.value += byn.value;
-        return this;
-    }
-
-    public Byn sub(Byn byn) {
-        this.value -= byn.value;
-        return this;
-    }
-
-    public Byn mul(int k) {
-        this.value *= k;
-        return this;
-    }
-
-    private static int rounding(double roundingValue, Round round, int d) {
-        return (int) round.roundFunction(roundingValue / tenPowD[d]) * tenPowD[d];
-    }
-
-    public Byn mul(double k, Round round, int d) {
-        this.value = rounding(this.value * k, round, d);
-        return this;
-    }
-
-    public Byn mul(double k, int d) {
-        mul(k, Round.ROUND, d);
-        return this;
-    }
-
-    public Byn mul(double k) {
-        mul(k, Round.ROUND, 0);
-        return this;
-    }
-
-    public Byn mul(double k, Round round) {
-        this.value = rounding(this.value * k, round, 0);
-        return this;
-    }
-
-    public Byn round(Round round, int d) {
-        this.value = rounding(this.value, round, d);
-        return this;
-    }
-
-    public Byn round(Round round) {
-        this.value = rounding(value, round, 0);
-        return this;
-    }
-
-    public Byn round(int d) {
-        this.value = rounding(this.value, Round.ROUND, d);
-        return this;
-    }
-
-    public enum Round {
-        CEIL {
-            double roundFunction(double d) {
-                return Math.ceil(d);
-            }
-        },
-        FLOOR {
-            double roundFunction(double d) {
-                return Math.floor(d);
-            }
-        },
-        ROUND {
-            double roundFunction(double d) {
-                return Math.round(d);
-            }
-        };
-
-        abstract double roundFunction(double d);
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -116,5 +40,82 @@ public class Byn implements Comparable<Byn> {
     @Override
     public String toString() {
         return getRubs() + "." + value / 10 % 10 + value % 10;
+    }
+
+    public Byn add(Byn byn) {
+        this.value += byn.value;
+        return this;
+    }
+
+    public Byn sub(Byn byn) {
+        this.value -= byn.value;
+        return this;
+    }
+
+    public Byn mul(int k) {
+        this.value *= k;
+        return this;
+    }
+
+    public Byn mul(double k, Round round, int digits) {
+        this.value = round.rounding(this.value * k, digits);
+        return this;
+    }
+
+    public Byn mul(double k, int digits) {
+        mul(k, Round.ROUND, digits);
+        return this;
+    }
+
+    public Byn mul(double k) {
+        mul(k, Round.ROUND, 0);
+        return this;
+    }
+
+    public Byn mul(double k, Round round) {
+        this.value = round.rounding(this.value * k, 0);
+        return this;
+    }
+
+    public Byn round(Round round) {
+        this.value = round.rounding(this.value, 0);
+        return this;
+    }
+
+    public Byn round(int digits) {
+        Round round = Round.ROUND;
+        this.value = round.rounding(this.value, digits);
+        return this;
+    }
+
+    public Byn round(Round round, int digits) {
+        this.value = round.rounding(this.value, digits);
+        return this;
+    }
+
+    public static enum Round {
+        CEIL {
+            double roundFunction(double roundingValue) {
+                return Math.ceil(roundingValue);
+            }
+        },
+        FLOOR {
+            double roundFunction(double roundingValue) {
+                return Math.floor(roundingValue);
+            }
+        },
+        ROUND {
+            double roundFunction(double roundingValue) {
+                return Math.round(roundingValue);
+            }
+        };
+
+        abstract double roundFunction(double roundingValue);
+
+        private int[] tenPowD = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+
+        private int rounding(double roundingValue, int digits) {
+            return (int) roundFunction(roundingValue / tenPowD[digits]) * tenPowD[digits];
+        }
     }
 }
