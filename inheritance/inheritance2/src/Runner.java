79f7d34e645f169a56abb9b98a6dbe8f1ce3e475
VlaShip <vlaship@bk.ru>
2018-04-14 21:58:09 +0300

    add new search to inheritance2

--- inheritance/inheritance2/src/Runner.java
+++ inheritance/inheritance2/src/Runner.java
@@ -7,16 +7,24 @@ public class Runner {
     public static void main(String[] args) {
 
         // 1. Create the unique product for purchasing.
-        final Product MILK = new Product("Milk", new Byn(234));
+        final Product MILK = new Product("Milk", new Byn(10));
 
         // 2. Create an array for 6 objects
-        AbstractPurchase[] purchases = new AbstractPurchase[]{
+        /*AbstractPurchase[] purchases = new AbstractPurchase[]{
                 new TransportExpensesPurchase(MILK, 7, 753),
                 new PercentDiscountPurchase(MILK, 8, 4.12),
                 new PriceDiscountPurchase(MILK, 6, 23),
                 new PercentDiscountPurchase(MILK, 15, 2.56),
                 new TransportExpensesPurchase(MILK, 1, 123),
                 new PriceDiscountPurchase(MILK, 3, 55)
+        };*/
+
+        AbstractPurchase[] purchases = new AbstractPurchase[]{
+                new TransportExpensesPurchase(MILK, 1, 0),
+                new TransportExpensesPurchase(MILK, 2, 0),
+                new TransportExpensesPurchase(MILK, 2, 0),
+                new TransportExpensesPurchase(MILK, 2, 0),
+                new TransportExpensesPurchase(MILK, 1, 0),
         };
 
         // 3. Print the array content to the console
@@ -33,6 +41,10 @@ public class Runner {
 
         // 7. Find some purchase with cost equaled to 5.00 BYN
         purchaseWithCost(purchases);
+
+
+        allPurchaseWithCost(purchases);
+
     }
 
     private static void printPurchases(AbstractPurchase[] purchases) {
@@ -49,6 +61,46 @@ public class Runner {
             System.out.println("Required purchase is not found");
         } else {
             System.out.println("Required purchase is " + purchases[index]);
+        }
+    }
+
+    private static void allPurchaseWithCost(AbstractPurchase[] purchases) {
+        int max = 0;
+        int min = 0;
+        int current;
+
+        int index = Arrays.binarySearch(purchases, new TransportExpensesPurchase(
+                new Product(null, new Byn(500)), 1, 0));
+
+        if(index < 0) {
+            return;
+        }
+
+        current = index;
+
+        while (current > 0) {
+            if(purchases[index].getCost().equals(purchases[current-1].getCost())){
+                current--;
+            }
+            else {
+                min = current;
+                break;
+            }
+        }
+        current = index;
+
+        while (current < purchases.length) {
+            if(purchases[index].getCost().equals(purchases[current+1].getCost())){
+                current++;
+            }
+            else {
+                max = current;
+                break;
+            }
+        }
+
+        for (int i = min; i <= max; i++){
+            System.out.println(purchases[i]);
         }
     }
 }
