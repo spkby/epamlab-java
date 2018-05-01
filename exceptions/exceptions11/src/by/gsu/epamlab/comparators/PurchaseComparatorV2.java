1badb236fbe923123491c138b0b452cc43371d01
vlaship <vlaship@bk.ru>
2018-05-01 21:23:06 +0300

    exceptions11 PurchaseComparatorV2

---
+++ exceptions/exceptions11/src/by/gsu/epamlab/comparators/PurchaseComparatorV2.java
@@ -0,0 +1,13 @@
+package by.gsu.epamlab.comparators;
+
+import by.gsu.epamlab.entity.Purchase;
+
+import java.util.Comparator;
+
+public class PurchaseComparatorV2 implements Comparator<Purchase> {
+
+    @Override
+    public int compare(Purchase o1, Purchase o2) {
+        return 0;
+    }
+}
