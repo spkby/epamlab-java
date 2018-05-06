8b56f47f543ea20060668de6ac0ac0dcefdd692c
vlaship <vlaship@bk.ru>
2018-05-06 23:19:33 +0300

    exceptions12: working

--- exceptions/exceptions12/src/by/gsu/epamlab/exceptions/NonpositiveArgumentException.java
+++ exceptions/exceptions12/src/by/gsu/epamlab/exceptions/NonpositiveArgumentException.java
@@ -1,12 +1,33 @@
 package by.gsu.epamlab.exceptions;
 
 public class NonpositiveArgumentException extends IllegalArgumentException {
+
+    private static String MESSAGE = "Non positive value";
+
+    private final String field;
+    private final int value;
+
+    public NonpositiveArgumentException(int nonpositiveValue, String fieldName) {
+
+        value = nonpositiveValue;
+        field = fieldName;
+
+        System.err.println(MESSAGE + " " + value + " " + field);
+    }
+
+    /*
+    public NonpositiveArgumentException(int nonpositiveValue, NumField field) {
+
+    }
+
+    public NonpositiveArgumentException(NumField field) {
+
+    }*/
 }
 
 /*
-– public NonpositiveArgumentException(String cause) – самый простой и одновременно плохой вариант.
-– public NonpositiveArgumentException(int nonpositiveValue, String fieldName) – вариант без контроля поля на этапе трансляции.
-– public NonpositiveArgumentException(int nonpositiveValue, NumField field) – вариант с раздельным хранением недопустимого значения и контролем поля.
-– public NonpositiveArgumentException(NumField field) – вариант с контролем поля, которое хранит недопустимое значение.
-
+–  – самый простой и одновременно плохой вариант.
+–  – вариант без контроля поля на этапе трансляции.
+–  – вариант с раздельным хранением недопустимого значения и контролем поля.
+–  – вариант с контролем поля, которое хранит недопустимое значение.
  */
